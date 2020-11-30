c593370df61d372baeace797659b6e83b3538e6d
Peleg Z <peleg122@gmail.com>
2020-11-30 20:09:09 +0200

    Customer Area Page Creation and Strings added to strings.xml

---
+++ app/src/main/java/userlogina/example/mylastapplication/ShoppingArea.java
@@ -0,0 +1,14 @@
+package userlogina.example.mylastapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShoppingArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_area);
+    }
+}
