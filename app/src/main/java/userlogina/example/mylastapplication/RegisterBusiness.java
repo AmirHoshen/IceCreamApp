6a116850e22f6fce55a1f4850fac5ccab3f6147c
amir hoshen <mi.rosh160@gmail.com>
2020-11-30 12:57:19 +0200

    business and customer logged in properly to firebase database. need to build one more database for generating dished and so on.

--- app/src/main/java/userlogina/example/mylastapplication/RegisterBusiness.java
+++ app/src/main/java/userlogina/example/mylastapplication/RegisterBusiness.java
@@ -17,66 +17,76 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterBusiness extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
     private TextView banner, registerBusiness;
-    private EditText editTextFullName, editTextEmail, editTextPhone, editTextPassword;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+//    DatabaseReference myRef = database.getReference();
+
+    private EditText editTextBusinessOwnFullName, editTextBusinessName,editTextEmail, editTextPhone, editTextPassword;
     private ProgressBar progressBar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_customer);
+        setContentView(R.layout.activity_register_business);
 
         mAuth = FirebaseAuth.getInstance();
 
-        banner = (TextView)findViewById(R.id.bannerRegCustomer);
+        banner = (TextView)findViewById(R.id.bannerRegBusiness);
         banner.setOnClickListener(this);
 
-        registerBusiness = (Button)findViewById(R.id.buttonRegNewCustomer);
+        registerBusiness = (Button)findViewById(R.id.buttonRegNewBusiness);
         registerBusiness.setOnClickListener(this);
 
-        editTextFullName = (EditText)findViewById(R.id.editTextTextPersonNameRegCustomer);
-        editTextEmail = (EditText)findViewById(R.id.editTextTextEmailAddressRegCus);
-        editTextPhone = (EditText)findViewById(R.id.editTextPhoneRegCus);
-        editTextPassword = (EditText)findViewById(R.id.editTextTextPasswordRegCus);
+        editTextBusinessOwnFullName = (EditText)findViewById(R.id.editTextBusinessOwnerName);
+        editTextBusinessName = (EditText)findViewById(R.id.editTextBusinessName);
+        editTextEmail = (EditText)findViewById(R.id.editTextBusinessEmailAddress);
+        editTextPhone = (EditText)findViewById(R.id.editTextBusinessPhone);
+        editTextPassword = (EditText)findViewById(R.id.editTextBusinessPassword);
 
-        progressBar = (ProgressBar)findViewById(R.id.progressBar2);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar3);
 
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
-            case R.id.bannerRegCustomer:
+            case R.id.bannerRegBusiness:
                 startActivity(new Intent(this, MainActivityUserReg.class));
                 break;
-            case R.id.buttonRegNewCustomer:
+            case R.id.buttonRegNewBusiness:
                 registerUser();
         }
     }
 
     private void registerUser() {
-        String fullName, email, phone, password;
+        String businessOwnerFullName, businessName,email, phone, password;
 
-        fullName = editTextFullName.getText().toString().trim();
+        businessOwnerFullName = editTextBusinessOwnFullName.getText().toString().trim();
+        businessName = editTextBusinessName.getText().toString().trim();
         email =  editTextEmail.getText().toString().trim();
         phone = editTextPhone.getText().toString().trim();
         password = editTextPassword.getText().toString().trim();
 
-        if(fullName.isEmpty()){
-            editTextFullName.setError("Full name is required!");
-            editTextFullName.requestFocus();
+        if(businessOwnerFullName.isEmpty()){
+            editTextBusinessOwnFullName.setError("Full name is required!");
+            editTextBusinessOwnFullName.requestFocus();
             return;
         }
         if(phone.isEmpty()){
             editTextPhone.setError("Phone number is required!");
             editTextPhone.requestFocus();
             return;
+        }
+        if(businessName.isEmpty()){
+            editTextBusinessName.setError("Business name is required!");
+            editTextBusinessName.requestFocus();
         }
         if(email.isEmpty()){
             editTextEmail.setError("Email is required!");
@@ -104,12 +114,12 @@ public class RegisterBusiness extends AppCompatActivity implements View.OnClickL
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    User user = new User(fullName, email, phone, password);
+                    Business b = new Business(businessOwnerFullName, businessName,email, phone, password);
 
                     //return the id for the registered user
                     FirebaseDatabase.getInstance().getReference("Business")
                             .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            .setValue(b).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
 
