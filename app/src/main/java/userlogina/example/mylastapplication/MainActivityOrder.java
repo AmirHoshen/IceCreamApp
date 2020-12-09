3febd6431d8ff377233e57dabeb0b83168defe9e
amir hoshen <mi.rosh160@gmail.com>
2020-12-10 00:52:31 +0200

    2020_12_10_all_menus_fixed___need_toBuild_product_amount_as_spinner

--- app/src/main/java/userlogina/example/mylastapplication/MainActivityOrder.java
+++ app/src/main/java/userlogina/example/mylastapplication/MainActivityOrder.java
@@ -3,45 +3,12 @@ package userlogina.example.mylastapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivityOrder extends AppCompatActivity {
-
-
-    ImageView mainImageView;
-    TextView title, description;
-
-    String data1, data2;
-    int myImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_order);
-
-        mainImageView = findViewById(R.id.imageViewBJOrder);
-        title = findViewById(R.id.title);
-        description = findViewById(R.id.description);
-
-        getData();
-        setData();
-    }
-
-    private void getData(){
-        if(getIntent().hasExtra("myImage") && getIntent().hasExtra("data1") && getIntent().hasExtra("data2")){
-            data1 = getIntent().getStringExtra(data1);
-            data2 = getIntent().getStringExtra(data2);
-            myImage = getIntent().getIntExtra("myImage", 1);
-        }else{
-            Toast.makeText(this, "No Data you need to contact programmers dept.", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void setData(){
-        title.setText(data1);
-        description.setText(data2);
-        mainImageView.setImageResource(myImage);
     }
 }
