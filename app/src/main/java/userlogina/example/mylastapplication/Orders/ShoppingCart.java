04bab3879626efb25c004e71df9058f264bbac5c
amir hoshen <mi.rosh160@gmail.com>
2020-12-12 19:40:53 +0200

    user can add products into his own firebase.

--- app/src/main/java/userlogina/example/mylastapplication/Orders/ShoppingCart.java
+++ app/src/main/java/userlogina/example/mylastapplication/Orders/ShoppingCart.java
@@ -4,22 +4,23 @@ import java.util.ArrayList;
 
 public class ShoppingCart {
 
-    ArrayList<Product> order_products;
+    ArrayList<Product> order_dish;
     int num_of_products;
+    double total_price;
 
     public ShoppingCart(){}
 
     public ShoppingCart(ArrayList<Product> order_products, int num_of_products) {
-        this.order_products = order_products;
+        this.order_dish = order_products;
         this.num_of_products = num_of_products;
     }
 
-    public ArrayList<Product> getOrder_products() {
-        return order_products;
+    public ArrayList<Product> getOrder_dish() {
+        return order_dish;
     }
 
-    public void setOrder_products(ArrayList<Product> order_products) {
-        this.order_products = order_products;
+    public void setOrder_dish(ArrayList<Product> order_dish) {
+        this.order_dish = order_dish;
     }
 
     public int getNum_of_products() {
--- app/src/main/java/userlogina/example/mylastapplication/SecondActivityOrderBenAndJerry.java
+++ app/src/main/java/userlogina/example/mylastapplication/SecondActivityOrderBenAndJerry.java
@@ -1,18 +1,44 @@
 package userlogina.example.mylastapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabItem;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import userlogina.example.mylastapplication.Orders.Dish;
+
 public class SecondActivityOrderBenAndJerry extends AppCompatActivity {
 
-    ImageView mainImageView;
-    TextView title, description;
+    private ImageView mainImageView;
+    private TextView title, description;
 
-    String data1, data2;
+    private Button addToCartBtn;
+
+    private FirebaseUser user;
+    private FirebaseDatabase database;
+    private DatabaseReference dbRef;
+
+    EditText dishPrice;
+    double price;
+    String dishTitle, dishDescription;
     int myImage;
 
     @Override
@@ -24,18 +50,50 @@ public class SecondActivityOrderBenAndJerry extends AppCompatActivity {
         mainImageView = findViewById(R.id.mainImageView);
         title = findViewById(R.id.orderTitle);
         description = findViewById(R.id.orderDescription);
+//        dishPrice = findViewById(R.id.priceTageDish);
 
         getData();
         setData();
 
+        addToCartBtn = findViewById(R.id.btnAddToCartBJ);
+
+        addToCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addToCart();
+            }
+        });
+
+    }
+
+    private void addToCart() {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        dbRef = database.getReference().child("Users").getRef().child("Shopping Cart").getRef();
+
+
+        dbRef.push().setValue(new Dish(title.getText().toString(),description.getText().toString(),29.9,1)).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(SecondActivityOrderBenAndJerry.this, "Dish added successfully to shopping cart", Toast.LENGTH_LONG).show();
+                    finish();
+
+                }else{
+                    Toast.makeText(SecondActivityOrderBenAndJerry.this, "Dish order has been canceled, please try again", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 
     private void getData(){
-        if(getIntent().hasExtra("myImage") && getIntent().hasExtra("data1") && getIntent().hasExtra("data2")){
+        if(getIntent().hasExtra("myImage") && getIntent().hasExtra("data1") && getIntent().hasExtra("data2") ){
 
-            data1 = getIntent().getStringExtra("data1");
-            data2 = getIntent().getStringExtra("data2");
+            dishTitle = getIntent().getStringExtra("data1");
+            dishDescription = getIntent().getStringExtra("data2");
             myImage = getIntent().getIntExtra("myImage",1);
+//            price = Double.parseDouble(dishPrice.getText().toString());
 
         }else{
             Toast.makeText(this, "No Data, please contact support",Toast.LENGTH_SHORT).show();
@@ -43,8 +101,9 @@ public class SecondActivityOrderBenAndJerry extends AppCompatActivity {
     }
 
     private void setData(){
-        title.setText(data1);
-        description.setText(data2);
+        title.setText(dishTitle);
+        description.setText(dishDescription);
         mainImageView.setImageResource(myImage);
+
     }
 }
